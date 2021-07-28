e07db6d5b8c7dfa022882f6c04da98917d1cb418
pouannes <pierreouannes@gmail.com>
2021-07-28 18:55:33 +0200

    add test copmonent to git tracking

---
+++ components/TestComponent.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export const About = (): JSX.Element => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Welcome to the about page</p>
+    </div>
+  );
+};
+
+export default About;
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -16,6 +16,7 @@ import rehypeKatex from 'rehype-katex';
 import math from 'remark-math';
 
 import Layout, { WEBSITE_HOST_URL } from '../../components/Layout';
+import TestComponent from '../../components/TestComponent';
 
 import { MetaProps } from '../../types/layout';
 import { PostType } from '../../types/post';
@@ -29,6 +30,7 @@ const components = {
   Head,
   Image,
   Link,
+  TestComponent,
 };
 
 type PostPageProps = {
