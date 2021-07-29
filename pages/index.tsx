379893757150d417a3617aab161c534d5081db83
pouannes <pierreouannes@gmail.com>
2021-07-29 10:35:34 +0200

    remove boilerplate

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,21 +13,7 @@ type IndexProps = {
 export const Index = ({ posts }: IndexProps): JSX.Element => {
   return (
     <Layout>
-      <h1>Home Page</h1>
-      <p>Next.js starter for your next blog or personal site. Built with:</p>
-      <ul className="list-disc pl-4 my-6">
-        <li>Next.js</li>
-        <li className="mt-2">Typescript</li>
-        <li className="mt-2">MDX</li>
-        <li className="mt-2">Tailwind CSS</li>
-      </ul>
-
-      <a
-        href="https://github.com/ChangoMan/nextjs-typescript-mdx-blog"
-        className="inline-block px-7 py-3 rounded-md text-white dark:text-white bg-blue-600 hover:bg-blue-700 hover:text-white dark:hover:text-white"
-      >
-        Get the source code!
-      </a>
+      <h1>Blog</h1>
 
       {posts.map((post) => (
         <article key={post.slug} className="mt-12">
