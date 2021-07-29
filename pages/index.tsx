9c14a07c473053977efce4c3fe9599c9499bc4e4
pouannes <pierreouannes@gmail.com>
2021-07-29 10:57:34 +0200

    make whole article section a link

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,24 +16,18 @@ export const Index = ({ posts }: IndexProps): JSX.Element => {
       <h1>Blog</h1>
 
       {posts.map((post) => (
-        <article key={post.slug} className="mt-12">
-          <p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
-            {format(parseISO(post.date), 'MMMM dd, yyyy')}
-          </p>
-          <h1 className="mb-2 text-xl">
-            <Link as={`/posts/${post.slug}`} href={`/posts/[slug]`}>
-              <a className="text-gray-900 dark:text-white dark:hover:text-blue-400">
-                {post.title}
-              </a>
-            </Link>
-          </h1>
-          <p className="mb-3">{post.description}</p>
-          <p>
-            <Link as={`/posts/${post.slug}`} href={`/posts/[slug]`}>
-              <a>Read More</a>
-            </Link>
-          </p>
-        </article>
+        <Link key={post.slug} as={`/posts/${post.slug}`} href={`/posts/[slug]`}>
+          <article className="mt-12 cursor-pointer">
+            <p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
+              {format(parseISO(post.date), 'MMMM dd, yyyy')}
+            </p>
+            <h1 className="mb-2 text-xl text-gray-900 dark:text-white ">
+              {post.title}
+            </h1>
+            <p className="mb-3">{post.description}</p>
+            <p></p>
+          </article>
+        </Link>
       ))}
     </Layout>
   );
