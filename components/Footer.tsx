24a9d4fc25ec9131ffa4aa2dff9807903416e16e
pouannes <pierreouannes@gmail.com>
2021-07-31 12:42:40 +0200

    add copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Footer = (): JSX.Element => {
   return (
-    <div className="flex flex-col sm:px-0 px-8 max-w-[44rem]  mx-auto border-t border-gray-200 dark:border-gray-600 py-8 mt-14 mb-20">
+    <footer className="flex flex-col sm:px-0 px-8 max-w-[44rem]  mx-auto border-t border-gray-200 dark:border-gray-600 py-8 mt-14 mb-20">
       <p className="text-gray-700 dark:text-gray-200">Pierre Ouannes</p>
       <div className="flex justify-between">
         <div className="flex flex-col">
@@ -38,7 +38,12 @@ const Footer = (): JSX.Element => {
           </StyledLink>
         </div>
       </div>
-    </div>
+      <p className="text-gray-500">
+        <small>
+          &copy; Copyright {new Date().getFullYear()}, Pierre Ouannes
+        </small>
+      </p>
+    </footer>
   );
 };
 
