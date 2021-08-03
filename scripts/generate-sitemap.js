b5e2bf5919f43a0368091f8a1660811a0a2e6caa
pouannes <pierreouannes@gmail.com>
2021-08-03 12:59:09 +0200

    fix sitemap generation

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -5,13 +5,15 @@ const prettier = require('prettier');
 
 (async () => {
   const prettierConfig = await prettier.resolveConfig('./.prettierrc.js');
-
-  // Ignore Next.js specific files (e.g., _app.js) and API routes.
   const pages = await globby([
-    'pages/**/*{.js,.mdx}',
+    'pages/*.js',
+    'pages/*.ts*',
+    'posts/**/*.mdx',
     '!pages/_*.js',
+    '!pages/_*.ts*',
     '!pages/api',
   ]);
+
   const sitemap = `
         <?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -19,13 +21,13 @@ const prettier = require('prettier');
               .map((page) => {
                 const path = page
                   .replace('pages', '')
+                  .replace('posts', '')
                   .replace('.js', '')
                   .replace('.mdx', '');
                 const route = path === '/index' ? '' : path;
-
                 return `
                         <url>
-                            <loc>${`https://yoursitehere.com${route}`}</loc>
+                            <loc>${`https://pierreouannes.com${route}`}</loc>
                         </url>
                     `;
               })
@@ -33,11 +35,11 @@ const prettier = require('prettier');
         </urlset>
     `;
 
-  // If you're not using Prettier, you can remove this.
   const formatted = prettier.format(sitemap, {
     ...prettierConfig,
     parser: 'html',
   });
 
+  // eslint-disable-next-line no-sync
   fs.writeFileSync('public/sitemap.xml', formatted);
 })();
